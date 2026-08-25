ed0367d4cc42952075dc78f6ea2627a7c69b5418
PIETRO ALVES PINTO <025966@fatecararas.com>
2026-08-25 11:34:35 -0300

    atualizacao das urls

--- StoreLink/projStoreLink/AppStoreLink/urls.py
+++ StoreLink/projStoreLink/AppStoreLink/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from AppStoreLink.views import index, perfil
+from AppStoreLink.views import index, perfil, loginEcadastro
 
 urlpatterns = [
     path('', index, name= 'index'),
     path('perfil', perfil,  name= 'perfil')
+    path('loginEcadastro', loginEcadastro,  name= 'loginEcadastro')
 ]
--- StoreLink/projStoreLink/projStoreLink/urls.py
+++ StoreLink/projStoreLink/projStoreLink/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppStoreLink.views import index,perfil
+from AppStoreLink.views import index, perfil, loginEcadastro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
