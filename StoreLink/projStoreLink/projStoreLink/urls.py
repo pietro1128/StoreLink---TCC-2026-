3aeac9467b4e510245375573d2e249c656bc28aa
RUAN CARLOS PEREIRAS <025851@fatecararas.com>
2026-08-28 09:19:53 -0300

    correção do urls.py

--- StoreLink/projStoreLink/projStoreLink/urls.py
+++ StoreLink/projStoreLink/projStoreLink/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppStoreLink.views import index, perfil, loginEcadastro
+from AppStoreLink.views import index, perfil, cadastro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
