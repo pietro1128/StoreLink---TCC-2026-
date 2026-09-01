6e7ad3763b82ed6dc2511cf2f15d88afe34e0e67
pereiraruan <pere1raru4n@gmail.com>
2026-08-31 21:47:53 -0300

    alteração na navbar, linkagem da tela inicial, tela de cadastro, tela de perfil e resolução das imagens não estarem aparecendo, adição do perfil do consumidor e mudança de perfil para perfil consumidor

--- StoreLink/projStoreLink/AppStoreLink/urls.py
+++ StoreLink/projStoreLink/AppStoreLink/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from AppStoreLink.views import index, perfil, cadastro, login
+from AppStoreLink.views import index, perfil_loja, perfil_consumidor, cadastro, login
 
 urlpatterns = [
     #pagina inicial
     path('', index, name= 'index'),
 
     #paginas de perfil
-    path('perfil', perfil,  name= 'perfil'),
+    path('perfil-loja', perfil_loja,  name= 'perfil-loja'),
+    path('perfil-consumidor', perfil_consumidor,  name= 'perfil-consumidor'),
 
     #paginas de Autenticação
     path('cadastro', cadastro,  name= 'cadastro'),
--- StoreLink/projStoreLink/AppStoreLink/views.py
+++ StoreLink/projStoreLink/AppStoreLink/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'AppStoreLink/index.html')
 
-def perfil(request):
-    return render(request, 'AppStoreLink/perfil.html')
+def perfil_loja(request):
+    return render(request, 'AppStoreLink/perfil-loja.html')
+
+def perfil_consumidor(request):
+    return render(request, 'AppStoreLink/perfil-consumidor.html')
 
 def cadastro(request):
     return render(request, 'registration/cadastro.html')
